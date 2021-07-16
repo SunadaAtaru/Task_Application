bc83a6618341d35ffee454a1f873c76da884ff76
EC2 Default User <ec2-user@ip-172-31-0-193.ec2.internal>
2021-07-16 14:57:05 +0000

    Task_Application cパート終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210622131245) do
+ActiveRecord::Schema.define(version: 20210716144623) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "description"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20210622131245) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
